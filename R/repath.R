34b2f66a532798352fd06f007733c0cd89d59ced
FFaupel <ffaupel@ufg.uni-kiel.de>
2018-10-10 18:46:30 +0200

    started to write functions

--- R/repath.R
+++ R/repath.R
@@ -4,44 +4,47 @@
 #' 
 #' @title repath
 #' 
-#' @param nodes_x a vector containing metric x coordinates of nodes
-#' @param nodes_y a vector containing metric y coordinates of nodes
-#' @param nodes_id a vector containing ID for nodes
-#' @param features a data.frame containing metric x and y coordinates of features, and feature type. Coordinates are expected to be the first two columns.
-#' @param type_col a character string naming the columname containing feature types.
-#' @param pre_size numeric, amount of letters, e.g. characters before typenumbers. Defaults to 1.
-#' @param method character string, the distance measure to be 
-#'   used ("euclidean", "maximum", "manhattan", "canberra", 
-#'   "binary" or "minkowski"). Defaults to euclidean distance.
-#' @param plotted a Boolean operator defining whether a plot should be created. Defaults to FALSE. Edge widths are scaled by maximum distance values and enlarged by factor 2. 
+#' @param df
+#' @param sgdf
+#' 
+#' @param x numeric, indicating column number of x coordinates
+#' @param y numeric, indicating column number of y coordinates
+#'  
 #' 
 #' @return a list containing a graph object of classes tidygraph ("tbl_graph") resp. igraph ("igraph") and the cultural distance matrix.
 #'         NOTE: The output igraph object contains zeros for display purpose. The cultural distance matrix (cult_dist_matr) is correct, containing NA for missing values.  
 #'
 #' @author Franziska Faupel <\email{ffaupel@@ufg.uni-kiel.de}>
-#' @author Oliver Nakoinz <\email{}>
+#' @author Oliver Nakoinz <\email{oliver.nakoinz.i@@gmail.com}>
 #' 
 #' @examples
 #' 
 #'
 #' @export 
 
-repath <- function(base_kde, nn, sgdf){
+repath <- function(df, sgdf, x = 1, y = 2){
   
+  pppm <- spatstat::ppp(df[,1], df[,2], 
+                         window = spatstat::owin(
+                           xrange=c(sgdf@bbox[1,1],sgdf@bbox[1,2]),
+                           yrange=c(sgdf@bbox[2,1],sgdf@bbox[2,2]), 
+                           unitname="m"))
+  f_sd1 <- sd1gen(pppm)
+  base_kde <- makestatkde(f_sd1[[1]], sgdf, df)
   #iter <- 2
   #rw     <- 500   # width of raster defined in m
   #tresh  <- 0.05  # treshold, to delete phaths in areas with really low density values (KDE), meaning calculation artefacts 
   #f_sd1  <- 4     # factor defining size if the first kernel, which generate the stucture of dynamic kernel
   #f1     <- 0.2      # factor defining the minimum border of dynamic kernel (raster width) f1*mean(nn)  ## 0.2
   #f2     <- 0.4      # factor defining the maximum border of dynamic kernel f2*mean(nn)
-  #f3     <- 0.5      # MinimalinentitÃÂ¤t des Kernels
-  #f4     <- 1        # MaximalinentitÃÂ¤t des Kernels
+  #f3     <- 0.5      # Minimalinentitt des Kernels
+  #f4     <- 1        # Maximalinentit des Kernels
   #s      <- -0.3     # Kernelparameter: incline starting from ponit 1
   #de     <- 0.7      # hight of additional kernell peak
   #sw     <- 12       # width of picture, cm
   #mwin   <- 9        # Mowing-window-size for ridge detection (4,9,16)
   #xp <- 750     # Kernelparameter: x-wert   von Punkt 1
-  
+  nn <- f_sd1[[2]]
   for(i  in 1:iter) {
     
     d2 <- min(base_kde@data$v)
@@ -106,4 +109,70 @@ repath <- function(base_kde, nn, sgdf){
   list_repath <- list(KDE=dyn_kde, ras_dens_ridges=ras_ridges_corr)
   
   return(list_repath)
+}
+
+#' Calculating Static KDE
+#' 
+#' description of function
+#' 
+#' @title makestatkde
+#' 
+#' @param pppm PointPattern object of Spatial Data
+#' @param f_sd1 factor defining size of the first kernel, which generate the stucture of dynamic kernel
+#' @param sgdf grid object to store results
+#' @param df data frame, containing coordinates of monuments
+#' @param x numeric, indicating column number of x coordinates
+#' @param y numeric, indicating column number of y coordinates
+#'  
+#' 
+#' @return SpatialGrid Object  
+#'
+#' @author Franziska Faupel <\email{ffaupel@@ufg.uni-kiel.de}>
+#' @author Oliver Nakoinz <\email{oliver.nakoinz.i@@gmail.com}>
+#' 
+#' @examples
+#' 
+#'
+#' @export
+
+makestatkde <- function(pppm, f_sd1=4, sgdf, df, x = 1, y = 2){
+  sd1 <- sd1gen(pppm)
+  sd1 <- sd1[[1]]
+  stest <- 0
+  if(stest != sd1) {
+    base_kde <- sgdf
+    for (i in seq(base_kde@data$v)){
+      pdist <- cbind(sp::coordinates(base_kde)[i,1],
+                     sp::coordinates(base_kde)[i,2],df[,x],df[,y])
+      base_kde@data$v[i]<- sum(apply(pdist,1,gau1,sd=sd1))
+    }
+  }
+  stest <- sd1
+  return(base_kde)
+}
+
+#' Factor defining size of first Kernel
+#' 
+#' factor defining size of the first kernel, which generate the stucture of dynamic kernel
+#' 
+#' @title sd1gen
+#' 
+#' @param pppm PointPattern Object of Data
+#' @param f_sd1 factor scaling the mean of nearest neighbourhoud 
+#'  
+#' 
+#' @return list containg two numeric  
+#'
+#' @author Franziska Faupel <\email{ffaupel@@ufg.uni-kiel.de}>
+#' 
+#' @examples
+#' 
+#'
+#' @export
+
+sd1gen <- function(pppm, f_sd1 = 4) {
+  sd1  <- f_sd1*mean(nndist(pppm))
+  nn <- mean(nndist(pppm))
+  sd1 <- list(sd1, nn)
+  return(sd1)
 }
