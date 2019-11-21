3aea53576104a14a8c1520c72a89c8bb1cf24230
H. Raese <h.raese@ufg.uni-kiel.de>
2019-11-21 18:36:27 +0100

    theoPath.R: - added function theoPath_herzog from FUN_theoPath.R - added function theoPath_param from FUN_theoPath.R - (line 50) renamed parameter ras_view to ras_para because it can be more than just a viewshed - (line 194) added default value to paramater r as per description -small correctiosn of wording
    Script for Testing.R:
    -expansion previous version
    - (line 32) new information on problems with width of Raster

--- R/theoPath.R
+++ R/theoPath.R
@@ -1,3 +1,110 @@
+#===============================================================================
+# Theoretical Paths using cost functions definded by Herzog 2012 
+#===============================================================================
+
+theoPath_herzog <- function(emp_ai, ras_ai, con, method, theta, p){
+  
+  ras_M1 <- emp_ai
+  # Loop to calculate rWalk for all connections
+  #-------------------------------------------------------------------------------
+  for(i in 1:length(con[,1])){
+    xmin <- con[i,5] - p 
+    xmax <- con[i,6] + p
+    ymin <- con[i,7] - p
+    ymax <- con[i,8] + p
+    ras <- raster::crop(ras_ai, raster::extent(xmin, xmax, ymin,ymax), filename= "corssings" , snap='near', overwrite=TRUE)
+    tran_hdiff<- gdistance::transition(ras, transitionFunction=hdiff, directions=8, symm=TRUE)
+    slope <- gdistance::geoCorrection(tran_hdiff, type="r", scl=TRUE)  
+    adj <- raster::adjacent(ras, cells=1:raster::ncell(ras), direction=8)                     # Setting up a adjacent matrix using 8 directions to construct a transition Layer to store cost values calculated in the next step. Attention: If 
+    cost_walki <- slope                                                   # storing Geo-Information in later TransitionLayer Object
+    
+    # Different Cost Functions
+    if(method == "walk_i"){
+      cost_walki[adj] <- walk_i(slope[adj])                          # Calculating cost surface using different functions/equation
+    }
+    
+    if(method == "drive_i"){
+      cost_walki[adj] <- drive_i(slope[adj])                          # Calculating cost surface using different functions/equation
+    }
+    
+    cost_walki <- gdistance::geoCorrection(cost_walki, type="r", scl=TRUE)                       # conductivity=cost/dist; time=1/conductivity
+    from <- data.frame(con[i, c(1,2)])
+    to <- data.frame(con[i, c(3,4)])
+    sp::coordinates(from)=~xFrom+yFrom; sp::coordinates(to)=~xTo+yTo; 
+    random <- gdistance::passage(cost_walki,from, to, theta = theta, totalNet="total") # rWalk: expectaion for a passage of a cell
+    ras_M1 <- raster::mosaic(ras_M1, random, fun=max) # summing up expactation of single rWalk connections
+  }
+  
+  plot(ras_M1)
+  
+  rm(random); rm(to); rm(from); rm(tran_hdiff); rm(slope); rm(cost_walki)
+  
+  return(ras_M1)
+  
+}
+
+#===============================================================================
+# Theoretical Paths with additional parameter and 2nd version with factor *1000
+#===============================================================================
+
+theoPath_param <- function(emp_ai, ras_ai, ras_para, con, method, theta){
+  
+  ras_M3 <- emp_ai
+  ras_M31000 <- emp_ai
+  # Loop to calculate rWalk for all connections
+  for(i in 1:length(con[,1])){
+    ## Setting area for randomwalk analysis of each connection
+    xmin <- con[i,5] - p 
+    xmax <- con[i,6] + p
+    ymin <- con[i,7] - p
+    ymax <- con[i,8] + p
+    from <- data.frame(con[i, c(1,2)])
+    to <- data.frame(con[i, c(3,4)])
+    sp::coordinates(from)=~xFrom+yFrom; sp::coordinates(to)=~xTo+yTo; 
+    ## Calculating a cost surface using viewwhed parameters
+    ras_v <- raster::crop(ras_view, raster::extent(xmin, xmax, ymin,ymax), filename= "corssings" , snap='near', overwrite=TRUE)
+    adj_v <- raster::adjacent(ras_v, cells=1:raster::ncell(ras_v), directions=8)
+    cost_v <- gdistance::transition(ras_v, transitionFunction=max, directions=8, symm=TRUE)
+    cost_v <- gdistance::geoCorrection(cost_v, type="r", scl=TRUE)
+    cost_v[adj_v] <- max(cost_v[adj_v])
+    ## Calculatting a cost surface using Elevation parameters
+    ras <- raster::crop(ras_ai, raster::extent(xmin, xmax, ymin,ymax), filename= "corssings" , snap='near', overwrite=TRUE)
+    tran_hdiff<- gdistance::transition(ras, transitionFunction=hdiff, directions=8, symm=TRUE)
+    slope <- gdistance::geoCorrection(tran_hdiff, type="r", scl=TRUE)
+    adj <- raster::adjacent(ras, cells=1:raster::ncell(ras), direction=8)# Setting up a adjacent matrix using 8 directions to construct a transition Layer to store cost values calculated in the next step
+    cost_walki <- slope                                                   # storing Geo-Information in later TransitionLayer Object
+    
+    # Different Cost Functions
+    if(method == "walk_i"){
+      cost_walki[adj] <- walk_i(slope[adj])                          # Calculating cost surface using different functions/equation
+    }
+    
+    if(method == "drive_i"){
+      cost_walki[adj] <- drive_i(slope[adj])                          # Calculating cost surface using different functions/equation
+    }
+    
+    cost_walki <- gdistance::geoCorrection(cost_walki, type="r", scl=TRUE)                       # conductivity=cost/dist; time=1/conductivity
+    ## Adding view-costs to elevation based cost surface
+    cost_walkia <- cost_walki + (cost_v*0.1)
+    cost_walkib <- cost_walki + (cost_v *100)
+    random <- gdistance::passage(cost_walkia,from, to, theta = theta, totalNet="total") # rWalk: expactaion for a passage of a cell
+    ras_M3 <- raster::mosaic(ras_M3, random, fun=max) # summing up expactation of single rWalk connections
+    random <- gdistance::passage(cost_walkib,from, to, theta = theta*10, totalNet="total") # rWalk: expactaion for a passage of a cell
+    ras_M31000 <- raster::mosaic(ras_M31000, random, fun=max) # summing up expactation of single rWalk connections
+  }
+  
+  plot(ras_M3)
+  plot(ras_M31000)
+  
+  rm(random); rm(to); rm(from); rm(tran_hdiff); rm(slope); rm(cost_walkia); rm(cost_walkib); rm(ras_v); rm(adj_v); rm(cost_v)
+  
+  FAC <- list("param"=ras_M3, "param_1000"=ras_M31000)
+  
+  return(FAC)
+  
+}
+
+
 #' Calculating a Delauny triangulation and preparing coordinates for rWalk loops
 #' 
 #' In absence of known contemporary nodes, like settlements, denser monument
@@ -84,7 +191,7 @@ theo_del <- function(maxima, win){
 #' @author Oliver Nakoinz <\email{oliver.nakoinz.i@@gmail.com}>
 #' 
 
-localMax <- function(df, x=1, y=2, r){
+localMax <- function(df, x=1, y=2, r=5000){
   sp_g <- sp::SpatialPoints(cbind(df[,1], df[,2]))
   # calculating static KDE
   bb <- sp::bbox(sp_g) # setting bondingbox for KDE
@@ -113,14 +220,14 @@ localMax <- function(df, x=1, y=2, r){
     indxy <- intersect(indx,indy)
     if (max(ras_dens@data[indxy,1]) == z & z > s)
     {indmax[length(indmax)+1] <- i}
-    #if (stats::sd(ras_dens@data[indxy,1]) == 0) #An dieser stelle rausgehauen - stattdessen ras_dens@data$v ?
+    if (stats::sd(ras_dens@data[indxy,1]) == 0) #An dieser stelle rausgehauen - stattdessen ras_dens@data$v ?
     if (stats::sd(ras_dens@data$v) == 0)
     {indplan[length(indplan)+1] <- i}
     rm(indx)
     rm(indy)
     rm(indxy)
   }
-  # exracting coordinates of local density maxmia
+  # extracting coordinates of local density maxmia
   mn      <- length(indmax)
   mx      <- sp::coordinates(ras_dens)[indmax,1]
   my      <- sp::coordinates(ras_dens)[indmax,2]
